c04183e8aba8f4d59096282d6bf954f0ba6d69ed
Maria Eduarda Scomparin <dudascomparin@gmail.com>
2026-03-04 20:06:22 -0300

    inicio

---
+++ src/main/java/com/mfs/milagre/MilagreApplication.java
@@ -0,0 +1,13 @@
+package com.mfs.milagre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MilagreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MilagreApplication.class, args);
+	}
+
+}
